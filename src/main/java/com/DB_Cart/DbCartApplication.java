8263478b4939e4594aa903d9393c0464b5b31cb0
DB_Cart-Admin <dbcartapplication@gmail.com>
2026-09-11 12:12:58 +0530

    Initialize DB_Cart Spring Boot application

---
+++ src/main/java/com/DB_Cart/DbCartApplication.java
@@ -0,0 +1,13 @@
+package com.DB_Cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbCartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DB_Cart/DbCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DB_Cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
